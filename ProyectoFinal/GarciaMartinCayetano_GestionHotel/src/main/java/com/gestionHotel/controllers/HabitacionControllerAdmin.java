be55f5ff1d60d9cd61d412f654bf8d3916c9360d
Cayetano García Martín <cayetanog2000@gmail.com>
2020-05-20 01:01:53 +0200

    habitaciones crear listo , falta mostrarlas en la lista

--- ProyectoFinal/GarciaMartinCayetano_GestionHotel/src/main/java/com/gestionHotel/controllers/HabitacionControllerAdmin.java
+++ ProyectoFinal/GarciaMartinCayetano_GestionHotel/src/main/java/com/gestionHotel/controllers/HabitacionControllerAdmin.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,31 +27,66 @@ public class HabitacionControllerAdmin {
 		
 		
 		@GetMapping("/gestion-habitaciones/list")
-		public String gestionhabitaciones() {
-		
+		public String gestionhabitacionesList(Model u) {
+			u.addAttribute("listaHabitaciones", servicioHab.findAll());
 			return "/admin/gHabitacionesLista";
 		}
 		
-		@GetMapping("/gestion-habitaciones")
-		public String gestionhabitaciones(Model u) {
+		//borrar
 		
+				@GetMapping("/gestion-habitaciones/borrar/{id}")
+				public String borrar(@PathVariable("id") long id) {
+					servicioHab.deleteById(id);
+					return "redirect:/admin/gestion-habitaciones/list";
+				}
+		
+		
+		//index
+		@GetMapping("/gestion-habitaciones")
+		public String indexgestionhabitaciones() {
+			
 			return "/admin/gHabitaciones";
 		}
-		
-		
-		@PostMapping("/gHabitaciones/submit")
-		public String anyadira(@ModelAttribute("addHabitacion") Habitacion u) { 
-			
-			if(u instanceof Individual) {
-				servicioHab.save(u);
-			}else if(u instanceof Doble) {
-				servicioHab.save(u);
-			}else if(u instanceof Familiar) {
-				servicioHab.save(u);
-			}
-		return "redirect:/admin/gestion-Habitaciones";
+		@GetMapping("/gHabitaciones/gHabitacionesIndividual")
+		public String indexgestionhabitacionesIndividual(Model m) {
+			m.addAttribute("addHabitacionInd", new Individual(0));
+			return "/admin/gHabitacionesIndividual";
 		}
-	
+		@GetMapping("/gHabitaciones/gHabitacionesDoble")
+		public String indexgestionhabitacionesDoble(Model m) {
+			m.addAttribute("addHabitacionDob", new Doble(0));
+			return "/admin/gHabitacionesDoble";
+		}
+		@GetMapping("/gHabitaciones/gHabitacionesFamiliar")
+		public String indexgestionhabitacionesFamiliar(Model m) {
+			m.addAttribute("addHabitacionFam", new Familiar(0));
+			return "/admin/gHabitacionesFamiliar";
+		}
+		//tipos
+		
+		
+		
+		//creaciones
+		
+		
+		@PostMapping("/gHabitaciones/gHabitacionesIndividual/submit")
+		public String addHabdInd(@ModelAttribute("addHabitacionInd") Individual u) { 
+			
+			servicioHab.save(u);
+		return "redirect:/admin/gestion-habitaciones";
+		}
+		@PostMapping("/gHabitaciones/gHabitacionesDoble/submit")
+		public String addHabdDob(@ModelAttribute("addHabitacionDob") Doble u) { 
+			
+			servicioHab.save(u);
+		return "redirect:/admin/gestion-habitaciones";
+		}
+		@PostMapping("/gHabitaciones/gHabitacionesFamiliar/submit")
+		public String addHabdFam(@ModelAttribute("addHabitacionFam") Familiar u) { 
+			
+			servicioHab.save(u);
+		return "redirect:/admin/gestion-habitaciones";
+		}
 	
 	
 }
--- ProyectoFinal/GarciaMartinCayetano_GestionHotel/src/main/java/com/gestionHotel/modelo/Usuario.java
+++ ProyectoFinal/GarciaMartinCayetano_GestionHotel/src/main/java/com/gestionHotel/modelo/Usuario.java
@@ -1,3 +1,4 @@
+
 package com.gestionHotel.modelo;
 
 import java.util.ArrayList;
